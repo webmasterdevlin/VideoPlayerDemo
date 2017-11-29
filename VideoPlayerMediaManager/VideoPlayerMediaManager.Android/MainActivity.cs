705d69e48db76ded64805baaf83c09fa2ea84647
webmasterdevlin <webmasterdevlin@gmail.com>
2017-11-29 22:11:49 +0800

    Add project files.

---
+++ VideoPlayerMediaManager/VideoPlayerMediaManager.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.MediaManager.Forms.Android;
+
+namespace VideoPlayerMediaManager.Droid
+{
+    [Activity(Label = "VideoPlayerMediaManager", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            VideoViewRenderer.Init();
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ VideoPlayerMediaManager/VideoPlayerMediaManager/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions.Enums;
+using Xamarin.Forms;
+
+namespace VideoPlayerMediaManager
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string VideoLink = "https://sec.ch9.ms/ch9/b721/db937a96-2fc1-4f27-85ce-a04ba510b721/K100.mp4";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ControlButton_OnClicked(object sender, EventArgs e)
+        {
+            switch (ControlButton.Text)
+            {
+                case "Play":
+                    await CrossMediaManager.Current.Play(VideoLink, MediaFileType.Video);
+                    ControlButton.Text = "Pause";
+                    ControlButton.BackgroundColor = Color.Crimson;
+                    VideoStatus.Text = "Playing...";
+                    break;
+                case "Pause":
+                    await CrossMediaManager.Current.Pause();
+                    ControlButton.Text = "Play";
+                    ControlButton.BackgroundColor = Color.LimeGreen;
+                    VideoStatus.Text = "Paused";
+                    break;
+            }
+        }
+    }
+}
